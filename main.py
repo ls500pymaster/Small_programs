bc25c2485f623ac1ef72c69392c407742ff61492
ls500pymaster <theone7877@gmail.com>
2022-07-08 12:49:31 +0300

    4 digit number in column using divmod

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-print("Hello World!")
-a = 1
-b = 2
-c = a + b
-print(c)
+number = int(input("Input 4 digit number: "))
+first, right_side = divmod(number, 10000)
+print(first)
+second, right_side = divmod(number, 1000)
+print(second)
+third, right_side = divmod(number, 100)
+fourth, right_side = divmod(number, 10)
+fifth, right_side = divmod(number, 1)
