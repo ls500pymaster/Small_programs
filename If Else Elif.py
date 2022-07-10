f453211a4f552f70ef9ab110317989102b910ad4
ls500pymaster <theone7877@gmail.com>
2022-07-10 21:43:11 +0300

    If, else, elif

--- If Else Elif.py
+++ If Else Elif.py
@@ -1,5 +1,11 @@
 number = int(input("Input any digit:"))
 
-if number >= 10:
+'''if number >= 10:
     print("Number is > 10")
-else: print("Number is < 1")
+else: print("Number is < 1")'''
+
+if number < 5:
+    print("Low")
+elif 5 <= number <= 10:
+    print("Mid")
+else: print("High")
