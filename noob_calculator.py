3d50aa11f5e5d2a366dc3af11684b6e16631efed
ls500pymaster <theone7877@gmail.com>
2022-07-12 19:04:58 +0300

    Noob calculator v. 0.1

---
+++ noob_calculator.py
@@ -0,0 +1,24 @@
+print("Hello. This is Noob Calculator")
+# User input
+number_1 = int(input("Please input first digit: "))
+number_2 = int(input("Please input second digit: "))
+# Math action input
+math_operation = input("Please select what operation do you want to do: + - / * ")
+if math_operation == "/":
+    # Checking div by zero
+    if not number_1 or number_2 == 0:
+        print("Please don't use zero.")
+    elif math_operation == "/":
+        res_div = int(number_1 / number_2)
+        print(res_div)
+elif math_operation == "*":
+    res_mult = number_1 * number_2
+    print(res_mult)
+elif math_operation == "+":
+    res_add = number_1 + number_2
+    print(res_add)
+elif math_operation == "-":
+    res_sub = number_1 - number_2
+    print(res_sub)
+else:
+    print("Error.")
