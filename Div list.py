209c0868d1600729a08a00fb66b98f4d029af127
ls500pymaster <theone7877@gmail.com>
2022-07-13 12:17:38 +0300

    List divider

---
+++ Div list.py
@@ -0,0 +1,20 @@
+# Empry list
+new_list = []
+# Input data
+input_string = input('Enter elements of a list separated by space: ')
+# Split
+user_list = input_string.split()
+# Check length of list
+len_list = (len(user_list))
+# Div len into 2 parts
+len_list_parts = len_list // 2
+# First and second part of the list
+first_half = user_list[:len_list_parts]
+second_half = user_list[len_list_parts:]
+# Check even or odd
+if len_list % 2 == 0:
+    new_list = [first_half] + [second_half]
+    print(new_list)
+else:
+    new_list = [second_half] + [first_half]
+    print(new_list)
