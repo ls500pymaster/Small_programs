df863362129251b5f8086a1f755b516735f01f60
ls500pymaster <theone7877@gmail.com>
2022-07-15 00:08:03 +0300

    Div list fixed_final

--- Div list.py
+++ Div list.py
@@ -8,17 +8,13 @@ user_list = input_string.split()
 len_list = (len(user_list))
 # Div list into 2 parts
 middle_index = len_list // 2
-# Two parts
-first_half = user_list[:middle_index]
-second_half = user_list[middle_index:]
-
 # Check even or odd
-if len(second_half) % 2 == 1:
+if len_list % 2 == 1:
     first_half = user_list[:middle_index + 1]
     second_half = user_list[middle_index + 1:]
     new_list = [first_half] + [second_half]
     print(new_list)
-elif len(first_half) % 2 == 0:
+elif len_list % 2 == 0:
     first_half = user_list[0:middle_index]
     second_half = user_list[middle_index:]
     new_list = [first_half] + [second_half]
