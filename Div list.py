d121e670f885ac1cd3a11dfd19a8308ec9d5b48e
ls500pymaster <theone7877@gmail.com>
2022-07-14 23:55:12 +0300

    Div list fix_still not working

--- Div list.py
+++ Div list.py
@@ -6,15 +6,20 @@ input_string = input("Enter elements of a list separated by space: ")
 user_list = input_string.split()
 # Check length of list
 len_list = (len(user_list))
-# Div len into 2 parts
-len_list_parts = (len_list // 2) + 1
-# First and second part of the list
-first_half = user_list[:len_list_parts]
-second_half = user_list[len_list_parts:]
+# Div list into 2 parts
+middle_index = len_list // 2
+# Two parts
+first_half = user_list[:middle_index]
+second_half = user_list[middle_index:]
+
 # Check even or odd
-if len_list % 2 == 1:
+if len(second_half) % 2 == 1:
+    first_half = user_list[:middle_index + 1]
+    second_half = user_list[middle_index + 1:]
     new_list = [first_half] + [second_half]
     print(new_list)
-else:
+elif len(first_half) % 2 == 0:
+    first_half = user_list[0:middle_index]
+    second_half = user_list[middle_index:]
     new_list = [first_half] + [second_half]
     print(new_list)
