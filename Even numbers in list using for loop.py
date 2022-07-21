4ffc6f9e4bdd26a3145a11b4842a210076faf7f3
ls500pymaster <theone7877@gmail.com>
2022-07-21 23:40:40 +0300

    Even numbers in list using loop

--- Even numbers in list using for loop.py
+++ Even numbers in list using for loop.py
@@ -1,17 +1,21 @@
 # New list for checking
-new_list = [1, 3, 5]
+new_list = [0]
 # Plug list
 empty_list = []
+new_list_even = []
 # Plug count
 count = 0
-# Plug last digit
-last_digit = 0
-# Loop
-for i in new_list:
-    if i % 2 == 0:
-        empty_list.append(i)
-        last_digit = new_list[-1]
+# Even
+if new_list == []:
+    print("0")
+else:
+    new_list_even = new_list[::2]
+    print (new_list)
+    # Loop
+    for i in new_list_even:
+        empty_list.append (i)
         count += i
-# Multi all digit and last digit
-mult = count * last_digit
-print(mult)
+    last_digit = new_list[-1]
+    # Multi all digit and last digit
+    mult = count * last_digit
+    print (mult)
