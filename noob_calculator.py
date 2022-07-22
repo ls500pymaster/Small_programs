343005f40dcb324bb8198ff2e03e7c28938fa0a8
ls500pymaster <theone7877@gmail.com>
2022-07-22 18:35:09 +0300

    Noob calculator optimized 0.1

--- noob_calculator.py
+++ noob_calculator.py
@@ -1,58 +1,32 @@
 print("Hello. This is Noob Calculator!")
-# User input
-number_1 = int(input("Please input first digit: "))
-number_2 = int(input("Please input second digit: "))
-# Math action input
-math_operation = input("Please select what operation do you want to do: + - / * ")
-while math_operation == "+" or "-" or "/" or "*":
+# Check var
+check_math = "y"
+# Y and N => lower
+check_math = check_math.lower()
+# Loop
+while check_math == "y":
+    number_1 = int(input("Please input first digit: "))
+    number_2 = int(input("Please input second digit: "))
+    math_operation = input("Please select what operation do you want to do: + - / * ")
     if math_operation == "/":
-        # Checking div by zero
+    # Checking div by zero
         if not number_1 or number_2 == 0:
             print ("Please don't use zero.")
         elif math_operation == "/":
             res_div = int (number_1 / number_2)
             print(res_div)
-            check_math = input("Do you want to continue? Type Y or N: ")
-            check_math = check_math.lower()
-            if check_math == "y":
-                number_1 = int (input ("Please input first digit: "))
-                number_2 = int (input ("Please input second digit: "))
-                math_operation = input ("Please select what operation do you want to do: + - / * ")
-            else:
-                break
+        check_math = input("Do you want to continue? ")
     elif math_operation == "*":
         res_mult = number_1 * number_2
         print(res_mult)
-        check_math = input ("Do you want to continue? Type Y or N: ")
-        check_math = check_math.lower ()
-        if check_math == "y":
-            number_1 = int (input ("Please input first digit: "))
-            number_2 = int (input ("Please input second digit: "))
-            math_operation = input ("Please select what operation do you want to do: + - / * ")
-        else:
-            break
+        check_math = input ("Do you want to continue? ")
     elif math_operation == "+":
         res_add = number_1 + number_2
         print(res_add)
-        check_math = input ("Do you want to continue? Type Y or N:")
-        check_math = check_math.lower ()
-        if check_math == "y":
-            number_1 = int (input ("Please input first digit: "))
-            number_2 = int (input ("Please input second digit: "))
-            math_operation = input ("Please select what operation do you want to do: + - / * ")
-        else:
-            break
+        check_math = input ("Do you want to continue? ")
     elif math_operation == "-":
         res_sub = number_1 - number_2
         print (res_sub)
-        check_math = input ("Do you want to continue? Type Y or N:")
-        check_math = check_math.lower ()
-        if check_math == "y":
-            number_1 = int (input ("Please input first digit: "))
-            number_2 = int (input ("Please input second digit: "))
-            math_operation = input ("Please select what operation do you want to do: + - / * ")
-        else:
-            break
+        check_math = input ("Do you want to continue? ")
     else:
         print("Error.")
-
