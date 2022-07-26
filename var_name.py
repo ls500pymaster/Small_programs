ea94c9e83fbd1587c3ae4b2df5d590a91bb705f0
ls500pymaster <theone7877@gmail.com>
2022-07-26 21:17:59 +0300

    var name ..

--- var_name.py
+++ var_name.py
@@ -2,25 +2,25 @@ import string
 from keyword import kwlist
 
 # Input
-check_var_name = input ("Input: ")
+check_var_name = input("Input: ")
 keyword_list = kwlist
-punctuation_list = string.punctuation.replace ("_", " ")
-# Loop exit
+
+punctuation_list: str = string.punctuation.replace("_", " ")
+
 loop_exit = True
 # While loop
-while loop_exit:
-    for i in punctuation_list:
-        if i not in check_var_name:
-            loop_exit = False
-            if not check_var_name[0].isdigit ():
-                loop_exit = False
-                if not check_var_name[0].isnumeric ():
-                    loop_exit = False
-                    if not check_var_name[0].isupper ():
-                        loop_exit = False
-                        if not check_var_name in keyword_list:
-                            print (loop_exit)
-                            break
 
-
-
+for i in keyword_list:
+    if i in check_var_name:
+        loop_exit = False
+    elif check_var_name[0].isdigit():
+        loop_exit = False
+    elif check_var_name.upper ():
+        loop_exit = False
+    elif check_var_name[0].isnumeric ():
+        loop_exit = False
+    elif check_var_name.count ('_') > 1:
+        loop_exit = False
+    elif check_var_name in punctuation_list:
+        loop_exit = False
+print(loop_exit)
