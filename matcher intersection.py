898ea520cd9aa75fbd24830bc757ba4671295282
ls500pymaster <theone7877@gmail.com>
2022-08-02 23:04:20 +0300

    999 mult

---
+++ matcher intersection.py
@@ -0,0 +1,11 @@
+import random
+# Generate random sets
+random_list_one = set(random.sample(range(100), 3))
+random_list_two = set(random.sample(range(100), 5))
+
+x = random_list_one
+y = random_list_one
+# Intersection for 2 sets
+z = x.intersection(y)
+print(z)
+print(x & y)
