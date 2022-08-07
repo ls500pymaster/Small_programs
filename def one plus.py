20bf25fd23d2f2b0353ee1a8545e69c4d20aff9c
ls500pymaster <theone7877@gmail.com>
2022-08-07 18:26:15 +0300

    def plus list

--- def one plus.py
+++ def one plus.py
@@ -2,6 +2,7 @@
 def calculate_summa(number_one, number_two):
     # Converting list to int
     result = int(''.join(map(str, number_one))) + number_two
-    return result
+    res = [int(x) for x in str(result)]
+    return res
 
 print(calculate_summa(list(input("Number one: ")), int(input("Number two: "))))
