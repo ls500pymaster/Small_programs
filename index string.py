8f62066831e87d675cd0be85fa82767684c7d088
ls500pymaster <theone7877@gmail.com>
2022-08-08 17:11:09 +0300

    index search fix

--- index string.py
+++ index string.py
@@ -3,7 +3,9 @@ input_one = input("Please input word: ")
 input_two = input("Please input char: ")
 
 first_check = input_one.find(input_two)
-if first_check == 0:
+second_check = input_one.find(input_two, first_check+1, len(input_one))
+
+if first_check == -1 or second_check == -1:
     print("None")
 else:
     second_check = input_one.find(input_two, first_check+1, len(input_one))
