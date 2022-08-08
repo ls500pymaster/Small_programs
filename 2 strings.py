6a1bc982468475755384fe293329ea75901499ea
ls500pymaster <theone7877@gmail.com>
2022-08-08 16:54:24 +0300

    999 mult

--- 2 strings.py
+++ 2 strings.py
@@ -1,33 +1,10 @@
 # Inputs
-input_one = input("Text: ")
-input_two = input("Chars: ")
+input_one = input("Please input word: ")
+input_two = input("Please input char: ")
 
-result = []
-exists = False
-n = 1
-
-# Enum tuple
-for index, char in enumerate(input_one):
-    if exists:
-        if n > len(input_two)-1:
-            exists = False
-            n = 1
-        elif char == input_two[n]:
-            n += 1
-        elif char != input_two[n]:
-            result = result[:-1]
-            n = 1
-            exists = False
-            # Checking chars
-    if char == input_two[0] and not exists:
-        exists = True
-        result.append(index)
-
-# Del first item
-del result[0]
-
-if result == []:
+first_check = input_one.find(input_two)
+if first_check == 0:
     print("None")
-# Converting list to str
 else:
-    print(int(''.join(map(str, result))))
+    second_check = input_one.find(input_two, first_check+1, len(input_one))
+    print(second_check)
