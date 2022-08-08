3611e06678397328d342668691fada6e027db515
ls500pymaster <theone7877@gmail.com>
2022-08-08 22:42:14 +0300

    unic element using count def

---
+++ unic element in list.py
@@ -0,0 +1,15 @@
+new_list = input("Input list of nums with spaces: ")
+new_list = new_list.split()
+empty_list = []
+empty_list_2 = []
+
+def unic_num(new_list):
+    for num in new_list:
+        if new_list.count(num) > 1:
+            empty_list.append(num)
+        else:
+            empty_list_2.append(num)
+            return empty_list_2
+
+print(" ".join(unic_num(new_list)))
+
