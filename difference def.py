78ca3483cbc2296cf4d231eb0a2d1afc73bd19b7
ls500pymaster <theone7877@gmail.com>
2022-08-10 21:42:08 +0300

    fix

--- difference def.py
+++ difference def.py
@@ -1,5 +1,8 @@
-new_list = [10.2, -2.2, 0, 1.1, 0.5]
+new_list = {10.2, -2.2, 0, 1.1, 0.5}
 def difference(new_list):
+    if new_list == {}:
+        print("0")
+        return
     max_number = max(new_list)
     min_number = min(new_list)
     result = max_number - min_number
