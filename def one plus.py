02f143c869e94bf4f06493e8162c92ead3d32ad6
ls500pymaster <theone7877@gmail.com>
2022-08-10 21:56:10 +0300

    fix

--- def one plus.py
+++ def one plus.py
@@ -1,8 +1,10 @@
-# New fun
-def calculate_summa(number_one, number_two):
+number_one = [9]
+print(type(number_one))
+def calculate_summa(number_one):
     # Converting list to int
+    number_two = 1
     result = int(''.join(map(str, number_one))) + number_two
     res = [int(x) for x in str(result)]
     return res
 
-print(calculate_summa(list(input("Number one: ")), 1))
+print(calculate_summa(number_one))
