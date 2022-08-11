94d07939536a3c4c4dd7c4f52a5236870bc411e0
ls500pymaster <theone7877@gmail.com>
2022-08-11 22:09:51 +0300

    fix

--- difference def.py
+++ difference def.py
@@ -1,11 +1,7 @@
-new_list = {10.2, -2.2, 0, 1.1, 0.5}
-def difference(new_list):
-    if new_list == {}:
-        print("0")
-        return
-    max_number = max(new_list)
-    min_number = min(new_list)
+def difference(*args, **kwargs):
+    max_number = max(*args, **kwargs)
+    min_number = min(*args, **kwargs)
     result = max_number - min_number
-    print((result.__round__(1)))
+    return ((result.__round__(1)))
 
-difference(new_list)
+print(difference(1, 2, 3))
