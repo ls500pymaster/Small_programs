22687b285cce04556c3912869bbd46041492127d
ls500pymaster <theone7877@gmail.com>
2022-08-12 14:46:42 +0300

    fix

--- difference def.py
+++ difference def.py
@@ -1,10 +1,9 @@
 def difference(*args):
-    if args == ():
-        return ("0")
+    if len(args) == 0:
+        return 0
     max_number = max(*args)
     min_number = min(*args)
     result = max_number - min_number
     return ((result.__round__(1)))
-
 
 print(difference())
